336afa002e8c73b6613ea3af9b0eb5e8a0f0d8b9
Aleksander Lukasiewicz <aleksander_lukasiewicz@outlook.com>
2023-02-11 22:43:03 +0100

    Fixed env

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import replace from '@rollup/plugin-replace';
+import { config } from 'dotenv';
 
+const configToReplace = {};
+
+for (const [key, value] of Object.entries(config().parsed)) {
+  configToReplace[`process.env.${key}`] = JSON.stringify(value);
+}
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  // Load app-level env vars to node-level env vars.
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    plugins: [svelte()],
-  })
-}
+export default defineConfig({
+  plugins: [svelte(),
+  replace(
+    {
+      include: ['./src/**/*.svelte', './src/**/*.js', './src/**/*.ts'],
+      preventAssignment: true,
+      values: configToReplace
+    }
+  )],
+})
 
