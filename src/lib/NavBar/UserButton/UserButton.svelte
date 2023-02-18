f95a7e8856bea72271e495136ca90e440c4ba271
Aleksander Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2023-02-18 14:42:27 +0100

    Moved user button into UserButton component

---
+++ src/lib/NavBar/UserButton/UserButton.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { ListItem,PersonPicture } from "fluent-svelte";
+    export let picture = "";
+    export let name="";
+</script>
+
+<ListItem id="user-button" on:click>
+    <PersonPicture size={20} src={picture} alt={name} />
+    <span id="user-label">{name || "Logging in"}</span>
+    <!-- <InfoBadge id="user-info" /> -->
+</ListItem>
+
+<style lang="scss">
+    @use "UserButton.scss";
+</style>
